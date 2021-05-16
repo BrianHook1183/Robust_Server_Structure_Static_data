75e822cefb96b00223c5cf9d8b36423f1c68722d
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-15 21:45:19 -0500

    submitted solution

--- src/app.js
+++ src/app.js
@@ -4,16 +4,58 @@ const app = express();
 const users = require("./data/users-data");
 const states = require("./data/states-data");
 
-// TODO: return an array of users from /users in form of { data: Array }
+//* ROUTES
 
-// TODO: return a single user by id from /users/:userId in form of { data: Object }
+// returns a single user by id from /users/:userId in form of { data: Object }
+app.use("/users/:userId", (req, res, next) => {
+  const { userId } = req.params;
+  const foundUser = users.find((user) => user.id === Number(userId));
 
-// TODO: return all states from /states in the form of { data: Array }
+  if (foundUser) {
+    res.json({ data: foundUser });
+  } else {
+    next(`User ID not found: ${userId}`);
+  }
+});
 
-// TODO: Return a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
+// returns an array of users from /users in form of { data: Array }
+app.use("/users", (req, res, next) => {
+  res.json({ data: users });
+});
 
-// TODO: add not found handler
+// returns a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
+app.use("/states/:stateCode", (req, res, next) => {
+  const { stateCode } = req.params;
+  const foundState = states[stateCode];
 
-// TODO: Add error handler
+  if (foundState === undefined) {
+    next(`State code not found: ${stateCode}`);
+  } else {
+    res.json({
+      data: {
+        stateCode: stateCode,
+        name: foundState,
+      },
+    }); // Return a JSON object, not a number.
+  }
+});
+
+// returns all states from /states in the form of { data: Array }
+app.use("/states", (req, res) => {
+  res.json({ data: states });
+});
+
+
+//* ERROR HANDLING
+
+//ROUTE NOT FOUND:
+app.use((req, res, next) => {
+  res.send(`Not found: ${req.originalUrl}`);
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  res.send(err);
+});
 
 module.exports = app;
